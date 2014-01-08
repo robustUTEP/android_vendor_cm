67d46e1b8adea6e464a4f4605dd492385c263f71
Eric Freudenthal <efreudenthal@utep.edu>
2014-01-08 11:37:20 -0700

    Update common.mk

--- config/common.mk
+++ config/common.mk
@@ -282,7 +282,7 @@ ifdef CM_BUILDTYPE
     endif
 else
     # If CM_BUILDTYPE is not defined, set to UNOFFICIAL
-    CM_BUILDTYPE := UNOFFICIAL
+    CM_BUILDTYPE := Snappy
     CM_EXTRAVERSION :=
 endif
 
